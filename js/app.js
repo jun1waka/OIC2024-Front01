9bfc72b742bcd05a1b04126b60f3c31263f8709e
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2024-05-20 10:00:18 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,19 +12,27 @@ const eltrump=document.querySelector("#trump");
 const card_type=['&spades;','&diams;','&hearts;','&clubs;']; 
 let HTML="";
 
-for(let i = 1 ; i <= 13; i++){
-	for(let j = 0 ; j <= 3 ; j++){
+for(let j = 0 ; j <= 3 ; j++){
+	const eltr = document.createElement('tr');
+	for(let i = 1 ; i <= 13; i++){
+		const eltd = document.createElemnt('td');
 	   console.log("i:"+i);
 	   console.log("j:"+j);
 	   cards[count] = new Card( card_type[j] , i );
 	   if(j==1 || j==2){
+	   		eltd.style.color = 'red';
+	   		eltd.innerHTML = `${card_type[j]}<br>${i}`;
 			HTML=HTML + `<p style="color:red"> ${card_type[j]}<br>${i}</p>`;
 		}else{
+	   		eltd.style.color = 'black';
+	   		eltd.innerHTML = `${card_type[j]}<br>${i}`;
 			HTML=HTML + `<p style="color:black"> ${card_type[j]}<br>${i}</p>`;
 		}
+		eltr.appendChild(eltd);
 	   count++;
 	}
+	eltrump.appendChild(eltr);
 }
-eltrump.innerHTML=HTML;
+//eltrump.innerHTML=HTML;
 // eltrump.innerHTML=`${card_type[2]}<br>${card_num}`;
 

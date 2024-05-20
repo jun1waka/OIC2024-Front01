d46a7779189712fe18c6dc8eb8274b547d7f6470
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2024-05-20 10:57:48 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,23 +12,12 @@ const eltrump=document.querySelector("#trump");
 const card_type=['&spades;','&diams;','&hearts;','&clubs;']; 
 
 for(let j = 0 ; j <= 3 ; j++){
-	const eltr = document.createElement('tr');
 	for(let i = 1 ; i <= 13; i++){
-		const eltd = document.createElement('td');
 	   console.log("i:"+i);
 	   console.log("j:"+j);
 	   cards[count] = new Card( card_type[j] , i );
-	   if(j==1 || j==2){
-	   		eltd.style.color = 'red';
-	   		eltd.innerHTML = `${ cards[count].type}<br>${ cards[count].num}`;
-		}else{
-	   		eltd.style.color = 'black';
-	   		eltd.innerHTML = `${ cards[count].type}<br>${ cards[count].num}`;
-		}
-		eltr.appendChild(eltd);
 	   count++;
 	}
-	eltrump.appendChild(eltr);
 }
 
 //shuffle
@@ -43,8 +32,27 @@ for(let i = 0;i < 52 ; i++){
 	//表示
 	console.log(`i = ${i},type= ${cards[i].type},num= ${cards[i].num}`);
 }
+//テーブル表示
+count＝０；
+for(let j = 0 ; j <= 3 ; j++){
+	const eltr = document.createElement('tr');
+	for(let i = 1 ; i <= 13; i++){
+		const eltd = document.createElement('td');
+	   console.log("i:"+i);
+	   console.log("j:"+j);
+	   if(j==1 || j==2){
+	   		eltd.style.color = 'red';
+	   		eltd.innerHTML = `${ cards[count].type}<br>${ cards[count].num}`;
+		}else{
+	   		eltd.style.color = 'black';
+	   		eltd.innerHTML = `${ cards[count].type}<br>${ cards[count].num}`;
+		}
+		eltr.appendChild(eltd);
+	   count++;
+	}
+	eltrump.appendChild(eltr);
+}
 
 
 //eltrump.innerHTML=HTML;
 // eltrump.innerHTML=`${card_type[2]}<br>${card_num}`;
-
